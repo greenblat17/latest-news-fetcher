c6bcd22b05d60a60b063ccb7ea0e6f6543fe3e57
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-05 17:47:44 +0300

    init news-fetcher module

---
+++ news-fetcher/src/main/java/dev/greenblat/newsfetcher/NewsFetcherApplication.java
@@ -0,0 +1,11 @@
+package dev.greenblat.newsfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFetcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFetcherApplication.class, args);
+    }
+}
