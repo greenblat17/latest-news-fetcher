0c56b48f0ff345a5532344077c5d060c86282bda
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-06 17:43:57 +0300

    ref: update entity for redis (#1)

--- news-fetcher/src/main/java/dev/greenblat/newsfetcher/dto/Article.java
+++ news-fetcher/src/main/java/dev/greenblat/newsfetcher/dto/Article.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Article {
+public class Article implements Serializable {
     private Source source;
     private String author;
     private String title;
